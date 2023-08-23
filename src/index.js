22df028fb0bd0673896507a83933468f508e523a
Leon Santana <leon.santana854@gmail.com>
2023-08-23 15:08:15 -0300

    Definindo escopo do projeto

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log("Projeto TODO");
+
+// Adicionar tarefas
+// Marcar uma tarefa como feita
+// Desmarcar uma tarefa como feita
+// Excluir tarefa
+// Editar titulo da tarefa
+// Exibir tarefas
